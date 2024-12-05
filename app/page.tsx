a1c3fac8f2a485af76d89fa06e065ff486137308
lohitchhabra <lohitchhabra@gmail.com>
2024-12-05 21:36:43 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header title="Hello world" />
+
+      // A typewriter letter at the end for personalization. mono font for typewriter
+      // effect, add a contact button within the letter for easy access
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div className="flex flex-row items-center justify-between p-5 bg-white/10 backdrop-blur-sm [mask-image:linear-gradient(to_bottom,white,transparent)]">
+            <a href="#">
+                <p className="text-2xl font-semibold">L</p>
+            </a>
+        </div>
+    )
+}
